b82c38f44e01a9b506656dd015a98fe8f0e9ed52
17018015@office.suwon.ac.kr <17018015@office.suwon.ac.kr>
2023-01-12 21:52:17 +0900

    Frist commit

---
+++ src/day01/MainClass02.java
@@ -0,0 +1,16 @@
+package day01;
+
+
+
+public class MainClass02 {
+	/**
+	 * JavaDoc 주석
+	 * 
+	 * 메인메소드 - 자바 처음 실행되는 메소드
+	 * @param args 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
+	 */
+	public static void main(String[] args) {
+		//Console 출력 명령어
+		System.out.println("안녕하세요");	
+	}
+}
